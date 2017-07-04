11d3a2b08e840b5e462361217f398a24ce3dec04
Adam Bowles <adam.bowles@nimvelo.com>
2017-07-04 21:00:41 +0100

    Done some changes

--- src/components/App/component.jsx
+++ src/components/App/component.jsx
@@ -1,17 +1,56 @@
 import React, { Component } from 'react'
 
+import classnames from 'classnames'
+import FontAwesome from 'react-fontawesome';
+
 import './style.css'
 
 export default class App extends Component {
   render () {
     return (
       <div className='site'>
-        <section className='intro'>
-          <div className='text'>
-            <h1>Adam Bowles</h1>
-            <h1>Front End Web Developer</h1>
+        <div className='info-block'>
+          <span className={classnames(['info', 'name'])}>Adam Bowles</span>
+          <span className={classnames(['info', 'job'])}>Front End Web Developer</span>
+          <div className={classnames(['info', 'links'])}>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.github.com/adambowles/'
+              className='link'
+              title='Github'
+            >
+              <FontAwesome name='github-square' fixedWidth />
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.twitter.com/adambowles'
+              className='link'
+              title='Twitter'
+            >
+              <FontAwesome name='twitter-square' fixedWidth />
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.linkedin.com/in/adambowles/'
+              className='link'
+              title='LinkedIn'
+            >
+              <FontAwesome name='linkedin-square' fixedWidth />
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='mailto:website@adambowl.es'
+              className='link'
+              title='Send me an email'
+            >
+              <FontAwesome name='envelope-square' fixedWidth />
+            </a>
           </div>
-        </section>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { render } from 'react-dom'
 import App from './components/App'
 
 import 'normalize.css'
-import './sanfran.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import './index.css'
 
 render(
